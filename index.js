85e5e8c6267f2396ba7cc9f02dbb874c5a75a88f
Dann Wee <dannweezj@gmail.com>
2023-09-05 22:17:39 +0700

    html-js wallet function

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const connectButton = document.getElementById("connectButton")
 const withdrawButton = document.getElementById("withdrawButton")
 const fundButton = document.getElementById("fundButton")
 const balanceButton = document.getElementById("balanceButton")
-connectButton.onclick = connect
+connectButton.onclick = connect // calls the connect function
 withdrawButton.onclick = withdraw
 fundButton.onclick = fund
 balanceButton.onclick = getBalance
 
 async function connect() {
-  if (typeof window.ethereum !== "undefined") {
+  if (typeof window.ethereum !== "undefined") { // check to see if window.ethereum (metamask) exists
     try {
-      await ethereum.request({ method: "eth_requestAccounts" })
+      await ethereum.request({ method: "eth_requestAccounts" }) // function that metamask object has that allows the website to see if there are accounts that it can send transactions to
     } catch (error) {
       console.log(error)
     }
@@ -45,16 +45,16 @@ async function withdraw() {
 }
 
 async function fund() {
-  const ethAmount = document.getElementById("ethAmount").value
+  const ethAmount = document.getElementById("ethAmount").value // gets the eth amount from the input field
   console.log(`Funding with ${ethAmount}...`)
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(contractAddress, abi, signer)
+  if (typeof window.ethereum !== "undefined") { // check to see if window.ethereum (metamask) exists
+    const provider = new ethers.providers.Web3Provider(window.ethereum) // gets the rpc url from inside our metamask --> use ethers
+    const signer = provider.getSigner() // allows us to grab this account form that is connected 
+    const contract = new ethers.Contract(contractAddress, abi, signer) // need a contract address, abi and a signer
     try {
       const transactionResponse = await contract.fund({
-        value: ethers.utils.parseEther(ethAmount),
-      })
+        value: ethers.utils.parseEther(ethAmount), // o.1eth --> 100000000000000000 
+      }) // what this function does is it sends this transaction to our metamask, so the website never accesses our private key
       await listenForTransactionMine(transactionResponse, provider)
     } catch (error) {
       console.log(error)
@@ -65,8 +65,8 @@ async function fund() {
 }
 
 async function getBalance() {
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+  if (typeof window.ethereum !== "undefined") { // check to see if window.ethereum (metamask) exists
+    const provider = new ethers.providers.Web3Provider(window.ethereum) // ethers is a javascript package that makes it easy to interact and work with metamask
     try {
       const balance = await provider.getBalance(contractAddress)
       console.log(ethers.utils.formatEther(balance))
